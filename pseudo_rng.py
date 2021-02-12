4cd10b5779d53726d2830d5e85db643a1389e3e6
esfinkel <finkelelisabeth@gmail.com>
2021-02-12 17:05:59 -0500

    code comments

--- pseudo_rng.py
+++ pseudo_rng.py
@@ -1,14 +1,26 @@
 import random, time
 
 def two_qubit_rng(entries=1000, noise=0.025):
+    '''Generate `entries` outputs simulating the Bell Circuit.
+    The two qubits have the same value `1-noise` percent of the time.'''
+    # seed the RNG so you get different results every time
     random.seed(time.time())
     vals = []
     for _ in range(entries):
+        # decide now if the two qubits will have the same value
+        # (simulating entanglement). This happens with probability `1-noise`.
         entangled = (random.random() > noise)
+        # The first qubit is 0 or 1 with equal probability
         fst = int(random.random() > 0.5)
+        # The second is the same as the first if they're entangled, and
+        # (for simplicity) the opposite otherwise
         snd = (fst if entangled else 1-fst)
+        # record the result
         val = str(fst) + str(snd)
         vals.append(val)
     return vals
 
-print(two_qubit_rng())
+res = two_qubit_rng()
+
+# confirm that '00' and '11' happen almost-but-not-quite equally frequently, and that an appropriate amount of noise is present
+print(res.count('00'), res.count('11'), res.count('10'))
