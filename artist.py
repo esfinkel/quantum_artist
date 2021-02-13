18917929c437701b245092db3c60b723b1d869ec
esfinkel <finkelelisabeth@gmail.com>
2021-02-13 10:38:15 -0500

    starting cleaning main

--- artist.py
+++ artist.py
@@ -1,5 +1,7 @@
 from graphics import *
-from fake_test import *
+import math
+
+import fake_test
 
 """
 Take in data and convert to good format.
@@ -25,72 +27,41 @@ def take_data(inputArray):
 # combine helpers together
 def main():
     # https://mcsp.wartburg.edu/zelle/python/graphics/graphics/index.html
-    import math
     win = GraphWin('Art', 200, 150) # give title and dimensions
     center = {'x': 100, 'y': 75}
 
-    data = take_data() # assuming list of tuples
+    data = take_data(fake_test.outputArray) # assuming list of tuples
     theta = 0
     r = 10
-    for (cbit, iobit, szbit, shbit) in data:
+    for i in len(data['firstArray']):
+        (cbit, iobit, szbit, shbit) = data['firstArray'][i], data['secondArray'][i], data['thirdArray'][i], data['fourthArray'][i]
         theta += 5 # or whatever
 
-        # # if circle
-        # r = max(10, r + size(szbit))
-        # x = r * math.cos(math.radians(theta))
-        # y = r * math.sin(math.radians(theta))
-        # dit = shape(shbit)(Point(x, y), r)
-        # dit.setFill(color(cbit))
-        # dit.draw(win)
-
-        # if triangle
         sz = size(szbit)
-        r = max(10, r + sz)
-        x = r * math.cos(math.radians(theta))
-        y = r * math.sin(math.radians(theta))
-        pt1 = Point(center['x']+x, center['y']+y)
-        x2 = (r*sz) * math.cos(math.radians(theta - 3))
-        y2 = (r*sz) * math.sin(math.radians(theta - 3))
-        pt2 = Point(center['x']+x2, center['y']+y2)
-        x3 = (r*sz) * math.cos(math.radians(theta + 3))
-        y3 = (r*sz) * math.sin(math.radians(theta + 3))
-        pt3 = Point(center['x']+x3, center['y']+y3)
-        dit = Polygon(pt1, pt2, pt3)
-        dit.setFill(color(cbit))
-        dit.draw(win)
-
-        # if line
-        sz = size(szbit)
-        r = max(10, r + sz)
+        io = inout(iobit)
+        r = max(10, r + io)
         x1 = r * math.cos(math.radians(theta))
         y1 = r * math.sin(math.radians(theta))
-        x2 = 2 * r * math.cos(math.radians(theta))
-        y2 = 2 * r * math.sin(math.radians(theta))
-        dit = Line(Point(center['x']+x1, center['y']+y1), Point(center['x']+x2, center['y']+y2))
-        dit.setWidth(sz)
+        sh = shape(shbit)
+
+        if sh=='triangle':
+            pt1 = Point(center['x']+x1, center['y']+y1)
+            x2 = (r*sz) * math.cos(math.radians(theta - 3))
+            y2 = (r*sz) * math.sin(math.radians(theta - 3))
+            pt2 = Point(center['x']+x2, center['y']+y2)
+            x3 = (r*sz) * math.cos(math.radians(theta + 3))
+            y3 = (r*sz) * math.sin(math.radians(theta + 3))
+            pt3 = Point(center['x']+x3, center['y']+y3)
+            dit = Polygon(pt1, pt2, pt3)
+
+        elif sh=='line':
+            x2 = 2 * r * math.cos(math.radians(theta))
+            y2 = 2 * r * math.sin(math.radians(theta))
+            dit = Line(Point(center['x']+x1, center['y']+y1), Point(center['x']+x2, center['y']+y2))
+            dit.setWidth(sz)
+
         dit.setFill(color(cbit))
         dit.draw(win)
-
-    # shape(i)(...)
-
-    '''
-
-    for i in range(points):
-    r = (data1[i%5] + 1) * 10
-    x = r * cos(radians(i*360/points)) + 50
-    y = r * sin(radians(i*360/points)) + 50
-    if data2[i%5]==1:
-        fill(153)
-    else:
-        fill(204, 102, 0)
-    size = (data3[i%5] + 1) * 2
-    if data4[i%5]==1:
-        triangle(x, y, x+size, y, x+(size/2), y+size)
-    else:
-        circle(x, y, size)
-
-
-    '''
 
     # ...
     win.getMouse()
@@ -100,18 +71,18 @@ def main():
 # make helpers
 def color(i):
     # take int, return color string
-    pass
+    return 'red'
 
 def inout(i):
     # take int, return coordinates?
-    pass
+    return 10
 
 def size(i):
     # return size
-    pass
+    return 5
 
 def shape(i):
-    return Circle # or something
+    return 'line'
     
 #extra helpers
 # The following four take Dictionary input (specifically from 'take_data(..)'
