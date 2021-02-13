6d6516059e8ceb6cae04c8b1541451be3d8c3aa3
Kshama Malavalli <kshamasridevi@gmail.com>
2021-02-13 10:37:13 -0500

    Change output of take_data to tuple from dictionary.

--- artist.py
+++ artist.py
@@ -4,9 +4,8 @@ from fake_test import *
 """
 Take in data and convert to good format.
 Input: Array 'outputArray' from 'fake_test.py' (array of 4-bit combinations).
-Output: Dictionary 'outputDict' containing four arrays where the nth array
-contains all nth position qubits of strings in the input array, in the form
-{'firstArray': [1,0,..0],..., 'fourthArray': [1,0,...0]}.
+Output: Tuple of 4 Arrays the nth array
+contains all nth position qubits of strings in the input array.
 """
 def take_data(inputArray):
     fstArray = []; sndArray = []; thdArray = []; fthArray = []
@@ -15,12 +14,7 @@ def take_data(inputArray):
         sndArray.append(int(str[1]))
         thdArray.append(int(str[2]))
         fthArray.append(int(str[3]))
-    outputDict = {'firstArray':fstArray, 'secondArray':sndArray,
-                  'thirdArray':thdArray, 'fourthArray':fthArray} #dictionary to retun all the qubit arrays.
-    return outputDict
-# Note: I wrote some extra helpers first (outputArray), second (...), etc. that
-# give each int array by itself. You can use them in main() or call this 
-# function  as take_data(outputArray).
+    return fstArray, sndArray, thdArray, fthArray
 
 # combine helpers together
 def main():
@@ -112,18 +106,6 @@ def size(i):
 
 def shape(i):
     return Circle # or something
-    
-#extra helpers
-# The following four take Dictionary input (specifically from 'take_data(..)'
-# and return an array of the nth qubits from the RNG output.
-def first(input):
-    return take_data(input).get('firstArray')
-def second(input):
-    return take_data(input).get('secondArray')
-def third(input):
-    return take_data(input).get('thirdArray')
-def fourth(input):
-    return take_data(input).get('fourthArray')
 
 #main()
 
