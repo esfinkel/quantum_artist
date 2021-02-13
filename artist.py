57b04815356b3aa98bfad116e1bfa2183aa3ec7b
esfinkel <finkelelisabeth@gmail.com>
2021-02-12 22:29:57 -0500

    drawer skeleton

---
+++ artist.py
@@ -0,0 +1,37 @@
+from graphics import *
+
+
+# take in data and convert to good format 
+def take_data():
+    pass
+
+
+# combine helpers together
+def main():
+    win = GraphWin('Art', 200, 150) # give title and dimensions
+
+    # shape(i)(...)
+
+    # ...
+    win.getMouse()
+    win.close()
+
+
+# make helpers
+def color(i):
+    # take int, return color string
+    pass
+
+def inout(i):
+    # take int, return coordinates?
+    pass
+
+def size(i):
+    # return size
+    pass
+
+def shape(i):
+    return Circle # or something
+    
+
+
